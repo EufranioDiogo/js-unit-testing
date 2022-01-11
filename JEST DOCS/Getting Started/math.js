9c819638d82683282774a2acbbb2edfa72c63b9e
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-01-11 21:24:10 +0100

    init: JEST DOCS

---
+++ JEST DOCS/Getting Started/math.js
@@ -0,0 +1,7 @@
+const sum = (number1 = 0, number2 = 0) => number1 + number2;
+const multiply = (number1 = 0, number2 = 0) => number1 * number2;
+const division = (number1 = 0, number2 = 0) => number1 / number2;
+const subtraction = (number1 = 0, number2 = 0) => number1 - number2;
+const restDivision = (number1 = 0, number2 = 0) => number1 % number2;
+
+module.exports = { sum, multiply, division, subtraction, restDivision };
